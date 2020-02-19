ad6b99178a23c025a95d11d3f6a6fed098c29464
linfu123 <1025595122@qq.com>
2020-02-19 11:54:31 +0800

    版本2.0

--- src/main/java/com/company/project/weixin/MenuKey.java
+++ src/main/java/com/company/project/weixin/MenuKey.java
@@ -21,7 +21,7 @@ public class MenuKey {
     public static final String SHARE_RECORD = "btn_share_record";
 
     //本地测试
-   public static final String URL = "http://fhfmb3.natappfree.cc/community/";
+   public static final String URL = "http://hvrnv6.natappfree.cc/community/";
     //智慧小区
    // public static final String URL = "http://f.pyblkj.cn/community/";
 
